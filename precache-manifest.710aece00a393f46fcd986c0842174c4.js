c425e1d944c484a0cbfbd0069916cd09690b9b56
arunkanil1994@gmail.com <arunkanil1994@gmail.com>
2022-05-04 22:22:46 +0530

    Updates

---
+++ precache-manifest.710aece00a393f46fcd986c0842174c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467f5e7b4559dd16941f7a8c68d03a2f",
+    "url": "/booking-manager/index.html"
+  },
+  {
+    "revision": "39892fbfddf2759019b6",
+    "url": "/booking-manager/static/css/2.e67327c7.chunk.css"
+  },
+  {
+    "revision": "989c7e023e829788cc16",
+    "url": "/booking-manager/static/css/main.b2046082.chunk.css"
+  },
+  {
+    "revision": "39892fbfddf2759019b6",
+    "url": "/booking-manager/static/js/2.6faf425a.chunk.js"
+  },
+  {
+    "revision": "989c7e023e829788cc16",
+    "url": "/booking-manager/static/js/main.f169d985.chunk.js"
+  },
+  {
+    "revision": "ffc8d24f0a895f0674af",
+    "url": "/booking-manager/static/js/runtime~main.0bde0f41.js"
+  },
+  {
+    "revision": "098ed6f5502a9715ee33dff0fe9b6bb5",
+    "url": "/booking-manager/static/media/pngwing.com.098ed6f5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7821774bc1bb45ec7e8f689e4c0f42fe.js"
+  "/booking-manager/precache-manifest.710aece00a393f46fcd986c0842174c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/booking-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
